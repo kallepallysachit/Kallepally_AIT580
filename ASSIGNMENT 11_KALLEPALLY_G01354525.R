3bef6384d1d8db84043da03adba5430be76c7a11
kallepallysachit <99299210+kallepallysachit@users.noreply.github.com>
2022-04-16 02:08:59 -0400

    Add files via upload

---
+++ ASSIGNMENT 11_KALLEPALLY_G01354525.R
@@ -0,0 +1,27 @@
+#TASK 1.a.
+#URL OF THE NEWS PAGE: https://www.espncricinfo.com/story/which-top-cricket-city-would-win-the-world-cup-1196522 
+
+The data visualization in the above URL shows the cities with the great bowling averages. The example has 
+the unneessary details in it. The top cities with the highest wickets has nothing to do with the teams that 
+will win the world cup. This is the problem in the data visualization in the the above URL. Instead of presenting
+the wickets taken by the cities, it can be represented in a better way by showing the wickets taken by the bowlers
+in each team. Hence this is the problem in the visualization and this is how it can be solves in a better way.
+
+
+#TASK 1.b.
+#URL OF THE NEWS PAGE: https://www.espncricinfo.com/story/which-top-cricket-city-would-win-the-world-cup-1196522
+
+The ESPN cricinfo has published an article on which cricket city will win the world cup in the year 2019.
+The data visualization present in the above URL represents the cricket places which has the best batting averages.
+It’s difficult to pick up any meaning from the data by looking at the graphics in the data. To get an insight into 
+what this visualization is all about, you’ll have to read the written texts before the graphics and the ones
+associated with it. The colour combinaton is also wrong in the visualization as it is not friendly to the eyes.
+You cannot categorize this visualization to any visual data visualization type. The closest it gets is to a bar chart, 
+but instead of bars, the designer used different shapes representing each city. Hence, this this the example of
+distortion and unneccessary detail in the visualization and this is the solution.
+
+REFERENCES : https://rigorousthemes.com/blog/bad-data-visualization-examples/  
+  
+
+
+  
